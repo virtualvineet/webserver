bc96088cf2986cb85807926b41e73cacc1c5e03c
user <user@ubuntu.(none)>
2013-06-14 02:35:55 -0400

    webserver

--- echo-server.py
+++ echo-server.py
@@ -34,17 +34,32 @@ while 1:
     client, address = s.accept()
     data = client.recv(size)
     print data
-   # client.send(data) 
     databr = data.split("\n")
     print databr[0]
     datafile = databr[0].split()
     filetoRead = datafile[1]
     
    # process_data(data)    
-    print 'Server:',data
-    f = open (filetoRead ,'r')
-    R = f.read()
-    print R
+    #print 'Server:',data
+    #f = open (filetoRead ,'r')
+    #R = f.read()
+    #print R
     #client.send("POST /home/vineet/PYTHON/index.html  HTTP/1.1")
-    client.send(R);
+    for dirname, dirnames, filenames in os.walk('.'):
+	for subdirname in dirnames:
+	   client.send(os.path.join(dirname, subdirname))
+	   client.send("\n")
+	   
+	for filename in filenames:
+	   client.send(os.path.join(dirname, filename))
+	   client.send("\n")
+       #for name in filename:
+      	#client.send(filename)
+      	#client.send('\n')
+
+
+
+    #client.send("<html>")
+    #client.send(R)
+    #client.send("</html>")
     client.close() 
